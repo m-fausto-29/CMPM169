31631b75cc60ab928a8b4adeffe88bbc7438db2f
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2024-01-25 17:37:01 -0800

    Integrating initial p5.js code

--- experiment3/js/sketch.js
+++ experiment3/js/sketch.js
@@ -1,29 +1,9 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Experiment3.js - Generative Methods
+// Author: Monserrat Fausto
+// Date: 1/25/2024
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
-
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
-}
 
 // setup() function is called once when the program starts
 function setup() {
@@ -37,31 +17,19 @@ function setup() {
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
     // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    //createCanvas(w = min(windowWidth, windowHeight), w);
+    w = min(windowWidth, windowHeight)
+    background(t = 0, 6, 60);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+    n = noise;
+    r = w / 2; 
+    a = n(t + 9) * r; 
+    b = n(w) * r; 
+    c = n(t) * 6;
+    d = n(t++ + 60) * 6;
+    stroke(w, 30);
+    line(r, a, cos(c) * a + r , sin(c) * b+r);
 }
