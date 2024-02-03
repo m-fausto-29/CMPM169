0211613982b7fc86ae3355d68197ca760ba1e270
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2024-02-02 21:28:50 -0800

    adding initial code and assets

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -5,24 +5,14 @@
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+let cdPlayer;
+let cd;
 
-// Globals
-let myInstance;
-let canvasContainer;
-
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+// Angle of rotation: the parameter that we'll animate!
+let angle = 0;
+function preload(){ //function necessary to preload the images
+  cdPlayer = createImg('cd_player.png', 'adding a cd player') // add the name of the image exactly, then the alternate title/description
+  cd = createImg('sony-cd-r.png', 'adding a cd')
 }
 
 // setup() function is called once when the program starts
@@ -36,32 +26,36 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
 
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    angleMode(DEGREES);
+  
+    // Background
+    for (let y = 0; y < height; y++) {
+        let inter = map(y, 0, height, 0, 1);
+        let c = lerpColor(color(255,192,203), color(135, 206, 235), inter);
+        stroke(c);
+        line(0, y, width,y); 
+    }
+    // cdPlayer.position(width/2, height/2); // adding the position so it appears on canvas
+    // cdPlayer.size(150, 150); // adjusts the width and height of the image
+    // cd.position(0, -10);
+    // cd.size(50, 50);
+    
 }
 
-// draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
+  //background(220);
+  imageMode(CENTER);
+  image(cdPlayer, width/2, height/2, 250, 250);
+  // We'll be drawing a square in the center of our sketch that rotates around itself
+  push();
+  translate(width/2, height/2);
 
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
-    noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
+  // The angle of rotation is what we're animating (ie changing over time) so first we'll increase its value and then rotate to that angle
+  angle += 1;
+  rotate(angle);
 
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+  // Try changing the amount we add to the angle and see how the animation changes! (Hint: we can think of this as 'speed')
+  imageMode(CENTER);
+  image(cd, 0, 0, 150, 150);
 }
