d13b5f6353718e7da4e21bcdf70e2c41d845c57f
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2024-02-05 20:08:53 -0800

    Update sketch.js

--- experiment4/js/sketch.js
+++ experiment4/js/sketch.js
@@ -34,7 +34,7 @@ function setup() {
 
     // Creating the play/pause button
   button = createButton('play');
-  button.position((width/2), 850);
+  button.position((width/2), 840);
   button.mousePressed(togglePlaying);
   
   // Initializing amplitude, frequency analysis, and angle mode
