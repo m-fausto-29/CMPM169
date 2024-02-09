060052b5fd9da5f2ec83e700a3ed2461919e34cf
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2024-02-08 18:55:17 -0800

    init experiment 5

--- experiment5/js/sketch.js
+++ experiment5/js/sketch.js
@@ -1,67 +1,62 @@
 // sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// Author: Monserrat Fausto
+// Date: 02/08/24
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
-
 // Globals
-let myInstance;
-let canvasContainer;
+let angle = 0;
+let star;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload(){
+    star = loadModel('assets/star.obj'); // preloading .obj file
 }
 
 // setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
+    background(220);
+    rectMode(CENTER);
     noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
+    
+    // Initializing values for smoother mouse control
+    let dx = mouseX - width/2;
+    let dy = mouseY - height/2;
+    let v = createVector(dx,dy,0);
+    
+    
+    // Ring Shape
+    push();
+    ambientLight(255,215,0); // gold ambient light
+    directionalLight(255,255,0,v); // directional light based on mouse control
+    specularMaterial(100, 240, 0);// yellow fill
+    rotateY(angle); // rotate along the y axis
+    torus(200, 45); // ring shaped figure
+    pop();
+    
+    // Star Shape
+    push();
+    ambientLight(200,0,0); // lighter red ambient light
+    directionalLight(210,0,0,v); // directional light based on mouse control
+    specularMaterial(255, 0, 0);// red fill
+    rotateX(55); // rotating its position to 55 radians
+    rotateZ(angle); // rotate along the z axis
+    scale(15, 15, 15); // adjusting model size
+    model(star); // inserting star.obj file
+    pop();
+    
+    angle += 0.07 // angle speed
 }
