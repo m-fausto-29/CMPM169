ab94ac840dda130f3266936916929ae6c241e646
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2024-02-17 16:30:09 -0800

    Adding initial experiment 6 code

--- experiment6/js/sketch.js
+++ experiment6/js/sketch.js
@@ -1,67 +1,156 @@
-// sketch.js - purpose and description here
-// Author: Your Name
-// Date:
+// sketch.js - Experiment 6: Grammars and Text Art 
+// Author: Monserrat Fausto
+// Date: 2/17/2024
 
 // Here is how you might set up an OOP p5.js project
 // Note that p5.js looks for a file called sketch.js
 
-// Constants - User-servicable parts
-// In a longer project I like to put these in a separate file
-const VALUE1 = 1;
-const VALUE2 = 2;
+let str = "The heart may be weak and sometimes it might even give in, but I've learned that deep down there's a light that never goes out";
+let str1 = "A scattered dream is like a far off memory, a far off memory is like a scattered dream...";
+let str_arr = [];
 
-// Globals
-let myInstance;
-let canvasContainer;
+let font;
 
-class MyClass {
-    constructor(param1, param2) {
-        this.property1 = param1;
-        this.property2 = param2;
-    }
-
-    myMethod() {
-        // code to run when method is called
-    }
+function preload() {
+  font = loadFont("Inconsolata.otf");
 }
 
 // setup() function is called once when the program starts
 function setup() {
     // place our canvas, making it fit our container
     canvasContainer = $("#canvas-container");
-    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height());
+    let canvas = createCanvas(canvasContainer.width(), canvasContainer.height(), WEBGL);
     canvas.parent("canvas-container");
     // resize canvas is the page is resized
     $(window).resize(function() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
-    // create an instance of the class
-    myInstance = new MyClass(VALUE1, VALUE2);
-
-    var centerHorz = windowWidth / 2;
-    var centerVert = windowHeight / 2;
+    let strs = str.split(" ");
+    for (let i = 0; i < strs.length*20; i++) {
+        let x = random(-(width * 2), width * 2);
+        let y = random(-(height * 2), height * 2);
+        let z = random(-width*5, width*2);
+        // let x = random(-(width / 2), width / 2);
+        // let y = random(-(height / 2), height / 2);
+        // let z = random(-width/5, width/2);
+        str_arr.push(new Type(strs[i%strs.length], x, y, z));
+    }
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
-    background(220);    
-    // call a method on the instance
-    myInstance.myMethod();
-
-    // Put drawings here
-    var centerHorz = canvasContainer.width() / 2 - 125;
-    var centerVert = canvasContainer.height() / 2 - 125;
-    fill(234, 31, 81);
+    background(0);
+    // Glowing Rectangle
+    push();
+    // drawingContext.shadowOffsetX = 5;
+    // drawingContext.shadowOffsetY = -5;
+    // drawingContext.shadowBlur = 10;
+    // drawingContext.shadowColor = 'blue';
+    // noFill();
+    // strokeWeight(20);
+    // rect(width*-50, -45, width*100, 90, 30);
+    ambientLight(155,155,200);
+    pointLight(135, 206, 235, width/2, height/2, 350);
+    specularMaterial(250);
+    shininess(1); 
     noStroke();
-    rect(centerHorz, centerVert, 250, 250);
-    fill(255);
-    textStyle(BOLD);
-    textSize(140);
-    text("p5*", centerHorz + 10, centerVert + 200);
-}
-
-// mousePressed() function is called once after every time a mouse button is pressed
-function mousePressed() {
-    // code to run when mouse is pressed
-}
+    plane(width*100, 250);
+    pop();
+    
+    // Plane
+    //directionalLight(100, 100, 250, width/2, height/2, -1);
+    push();
+    ambientLight(135, 206, 235);
+    pointLight(135, 206, 235, width/2, height/2, 330);
+    specularMaterial(250);
+    shininess(1); 
+    noStroke();
+    plane(width*100, 200);
+    pop();
+    
+    // // Glowing Rectangle
+    // push();
+    // // drawingContext.shadowOffsetX = 5;
+    // // drawingContext.shadowOffsetY = -5;
+    // // drawingContext.shadowBlur = 10;
+    // // drawingContext.shadowColor = 'blue';
+    // // noFill();
+    // // strokeWeight(20);
+    // // rect(width*-50, -45, width*100, 90, 30);
+    // ambientLight(10, 10, 100);
+    // pointLight(10, 10, 100, width/2, height/2, 50);
+    // specularMaterial(250);
+    // shininess(1); 
+    // noStroke();
+    // plane(width*100, 120);
+    // pop();
+    
+    orbitControl();
+    let count = str_arr.length;
+    for (let i = 0; i < str_arr.length; i++) {
+      str_arr[i].update();
+      str_arr[i].display();
+          count -= 1;
+    }
+    push()
+    // Centered Main Text
+    if(count == 0){
+      textAlign(CENTER);
+      textFont(font);
+      strokeWeight(3);
+      textSize(128);
+      //fill(0,0,100);
+      textNeon(color(135, 206, 235));
+      strokeWeight(3);
+      textSize(127);
+      fill(135, 206, 235)
+      text(str1, width/2, height/1.5);
+    }
+    pop()
+  }
+  function textNeon(glowColor){
+    glow(glowColor, 400);
+    text(str1, width/2, height/1.5);
+    text(str1, width/2, height/1.5);
+    glow(glowColor, 80);
+    text(str1, width/2, height/1.5);
+    text(str1, width/2, height/1.5);
+    glow(glowColor, 12);
+    text(str1, width/2, height/1.5);
+    text(str1, width/2, height/1.5);
+  }
+  
+  function glow(glowColor, blurriness){
+    drawingContext.shadowBlur = blurriness;
+    drawingContext.shadowColor = glowColor;
+  }
+  
+  class Type {
+    constructor(_str, _x, _y, _z) {
+      this.str = _str;
+      this.x = _x;
+      this.y = _y;
+      this.z = _z;
+    }
+  
+    update() {
+          this.z -= 10;
+      if(this.z > width/2){
+          this.z = width/2;
+      }
+    }
+  
+    display() {
+      push();
+      translate(this.x, this.y, this.z);
+      textAlign(CENTER, CENTER);
+      textFont(font);
+      textSize(100);
+      //fill(0,0,100);
+      //fill(random(128,128), random(238,144), random(230,255))
+      fill(random(100,255), random(100,255), random(100,255))
+      text(this.str, 0, 0);
+      pop();
+    }
+  }
