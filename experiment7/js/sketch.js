837abc5badc70098a85fe11b6de7765e1de75bd8
m-fausto-29 <129785531+m-fausto-29@users.noreply.github.com>
2024-02-25 22:02:49 -0800

    Update sketch.js

--- experiment7/js/sketch.js
+++ experiment7/js/sketch.js
@@ -7,6 +7,16 @@
 
 let data;
 let font;
+let metascore1;
+let metascore2;
+let metascore3;
+let metascore4;
+let metascore5;
+let userscore1;
+let userscore2;
+let userscore3;
+let userscore4;
+let userscore5;
 
 function preload(){
   data = loadTable('games.csv', 'csv', 'header');
@@ -24,36 +34,79 @@ function setup() {
         console.log("Resizing...");
         resizeCanvas(canvasContainer.width(), canvasContainer.height());
     });
+    // Meta Score Graphics
+    metascore1 = createGraphics(200, 200);
+    metascore1.textSize(75);
+    metascore2 = createGraphics(200, 200);
+    metascore2.textSize(75);
+    metascore3 = createGraphics(200, 200);
+    metascore3.textSize(75);
+    metascore4 = createGraphics(200, 200);
+    metascore4.textSize(75);
+    metascore5 = createGraphics(200, 200);
+    metascore5.textSize(75);
     
+    // User Score Graphics
+    userscore1 = createGraphics(200, 200);
+    userscore1.textSize(75);
+    userscore2 = createGraphics(200, 200);
+    userscore2.textSize(75);
+    userscore3 = createGraphics(200, 200);
+    userscore3.textSize(75);
+    userscore4 = createGraphics(200, 200);
+    userscore4.textSize(75);
+    userscore5 = createGraphics(200, 200);
+    userscore5.textSize(75);
 }
 
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
     background(220);
-  
-  
-  if(data){ // necessary for large data sets
-    // Get amount of rows in csv
-    let numRows = data.getRowCount();
+    // Red Background
+    push();
+    noStroke();
+    fill(255,0,0);
+    translate(0,-200);
+    plane(width, height/3);
+    pop();
     
-    // Get specific column
-    let game = data.getColumn(0);
-    let metaScore = data.getColumn(1);
-    let userScore = data.getColumn(2);
+    // Green Background
+    push();
+    noStroke();
+    fill(0,255,0);
+    plane(width, height/3);
+    pop();
     
-    for(let i = 0; i < numRows; i++){
+    if(data){ // necessary for large data sets
+        // Get amount of rows in csv
+        let numRows = data.getRowCount();
+        
+        // Get specific column
+        let game = data.getColumn(0);
+        let metaScore = data.getColumn(1);
+        let userScore = data.getColumn(2);
+        
+        for(let i = 0; i < numRows; i++){
         // Game 1
         push()
         fill(255,0,0);
         translate(-width / 3, -height / 3, 1);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        metascore1.text('95', 0, 100);
+        //pass image as texture
+        texture(metascore1);
         sphere(metaScore[0]);
         pop()
         
         push()
         fill(0,255,0);
         translate(-width / 3, -height / 50, 0)
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        userscore1.text('90', 0, 100);
+        //pass image as texture
+        texture(userscore1);
         sphere(userScore[0]);
         pop()
         
@@ -69,14 +122,22 @@ function draw() {
         push()
         fill(255,0,0);
         translate(-width / 6, -height / 3, 1);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        metascore2.text('94', 0, 100);
+        //pass image as texture
+        texture(metascore2);
         sphere(metaScore[1]);
         pop()
         
         push()
         fill(0,255,0);
         translate(-width / 6, -height / 50, 0);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        userscore2.text('82', 0, 100);
+        //pass image as texture
+        texture(userscore2);
         sphere(userScore[1]);
         pop()
         
@@ -92,14 +153,22 @@ function draw() {
         push()
         fill(255,0,0);
         translate(width / 40, -height / 3, 1);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        metascore3.text('94', 0, 100);
+        //pass image as texture
+        texture(metascore3);
         sphere(metaScore[2]);
         pop()
         
         push()
         fill(0,255,0);
         translate(width / 40, -height / 50, 0);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        userscore3.text('90', 0, 100);
+        //pass image as texture
+        texture(userscore3);
         sphere(userScore[2]);
         pop()
         
@@ -115,14 +184,22 @@ function draw() {
         push()
         fill(255,0,0);
         translate(width / 5, -height / 3, 1);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        metascore4.text('91', 0, 100);
+        //pass image as texture
+        texture(metascore4);
         sphere(metaScore[3]);
         pop()
         
         push()
         fill(0,255,0);
         translate(width / 5, -height / 50, 0);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        userscore4.text('87', 0, 100);
+        //pass image as texture
+        texture(userscore4);
         sphere(userScore[3]);
         pop()
         
@@ -138,14 +215,22 @@ function draw() {
         push()
         fill(255,0,0);
         translate(width / 2.7, -height / 3, 1);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        metascore5.text('90', 0, 100);
+        //pass image as texture
+        texture(metascore5);
         sphere(metaScore[4]);
         pop()
         
         push()
         fill(0,255,0);
         translate(width / 2.7, -height / 50, 0);
+        rotateX(5.6);
         rotateY(millis() / 1000);
+        userscore5.text('84', 0, 100);
+        //pass image as texture
+        texture(userscore5);
         sphere(userScore[4]);
         pop()
         
@@ -156,7 +241,7 @@ function draw() {
         fill(0,0,100);
         text("5. "+game[4], width/3.2, height/3)
         pop()
-     }
+        }
     }
   }
   
